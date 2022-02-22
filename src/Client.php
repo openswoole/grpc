23fc2f7200f154c88cb501eaa7af93725bfe5c90
Bruce Dou <doubaokun@gmail.com>
2022-02-22 15:56:16 +0000

    Updated h2 settings naming

--- src/Client.php
+++ src/Client.php
@@ -21,7 +21,8 @@ class Client
         'timeout'                => 2,
         'open_eof_check'         => true,
         'package_max_length'     => 2000000,
-        'max_concurrent_streams' => 1000,
+        'http2_max_concurrent_streams' => 1000,
+        'http2_max_frame_size' => 2 * 1024 * 1024,
         'max_retries'            => 30,
     ];
 
