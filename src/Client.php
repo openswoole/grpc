8f241c79bd03a844952e0530556cd856bf486f62
Bruce Dou <doubaokun@gmail.com>
2022-02-22 15:58:58 +0000

    Fix style

--- src/Client.php
+++ src/Client.php
@@ -18,12 +18,12 @@ class Client
     private $closed = false;
 
     private $settings = [
-        'timeout'                => 2,
-        'open_eof_check'         => true,
-        'package_max_length'     => 2000000,
+        'timeout'                      => 2,
+        'open_eof_check'               => true,
+        'package_max_length'           => 2000000,
         'http2_max_concurrent_streams' => 1000,
-        'http2_max_frame_size' => 2 * 1024 * 1024,
-        'max_retries'            => 30,
+        'http2_max_frame_size'         => 2 * 1024 * 1024,
+        'max_retries'                  => 30,
     ];
 
     public function __construct($host, $port)
