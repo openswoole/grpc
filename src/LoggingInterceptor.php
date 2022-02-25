248f7a17f684fdf733a2716d1c5eafd14d022704
Bruce Dou <doubaokun@gmail.com>
2022-02-25 15:30:50 +0000

    Example logger

--- src/LoggingInterceptor.php
+++ src/LoggingInterceptor.php
@@ -13,6 +13,12 @@ class LoggingInterceptor implements InterceptorInterface
 {
     public function handle(string $service, string $method, Context $context, $request, $invoker)
     {
+        $rawRequest = $context->getValue(\Swoole\Http\Request::class);
+        $client     = $rawRequest->server['remote_addr'] . ':' . $rawRequest->server['remote_port'];
+        $server     = $rawRequest->header['host'];
+        $streamId   = $rawRequest->streamId;
+        $ua         = $rawRequest->header['user-agent'];
+        \swoole_error_log(\SWOOLE_LOG_INFO, "GRPC request: {$client}->{$server}, stream({$streamId}), " . $service . '/' . $method . ', ' . $ua);
         return $invoker->handle($service, $method, $context, $request, $invoker);
     }
 }
